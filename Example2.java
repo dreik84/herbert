41b255d6805810dc93b365ad6efcd4790b40b224
dreik84 <dreik84@mail.ru>
2024-09-05 12:52:47 +0300

    update Example2.java

---
+++ Example2.java
@@ -0,0 +1,17 @@
+/*
+ * Демонстрация работы с переменными
+*/
+class Example2 {
+	public static void main(String[] args) {
+		int myVar1; // объявление переменной
+		int myVar2;
+
+		myVar1 = 1024; // присваивание переменной значения
+
+		System.out.println(myVar1);
+
+		myVar2 = myVar1 / 2;
+
+		System.out.println(myVar2);
+	}
+}
