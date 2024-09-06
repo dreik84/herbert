25b3f0271499a93b32f9b20355d4eb9ac861c508
dreik84 <dreik84@mail.ru>
2024-09-06 15:48:39 +0300

    update Example3.java

---
+++ Example3.java
@@ -0,0 +1,14 @@
+/*
+ * Отличия между int и double
+ */
+class Example3 {
+	public static void main(String[] args) {
+		int v;    // объявление целочисленной переменной
+		double x; // объявление переменной с плавующей точкой
+		v = 10;   // инициализация переменной
+		x = 10.0; // присваивание переменной значения
+		
+		System.out.println(v);
+		System.out.println(x);
+	}
+}
