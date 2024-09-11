a2736f0d7b1064650f9d67fd693fe83574324d3e
dreik84 <dreik84@mail.ru>
2024-09-11 15:44:09 +0300

    update ForDemo.java

---
+++ ForDemo.java
@@ -0,0 +1,13 @@
+/*
+ * Демонстрация использования цикла for
+ */
+class ForDemo {
+	public static void main(String[] args) {
+		int count;
+
+		for (count = 0; count < 5; count++) 
+			System.out.println(count);
+		
+		System.out.println("Готово!");
+	}
+}
