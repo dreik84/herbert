707ff9fa74d01b7513b0835688fa706bc3ce3f8a
dreik84 <dreik84@mail.ru>
2024-09-12 16:06:32 +0300

    update BlockDemo.java

--- BlockDemo.java
+++ BlockDemo.java
@@ -1,5 +1,18 @@
-class Example {
+/*
+ * Демонстрация использования блока кода
+ */
+class BlockDemo {
 	public static void main(String[] args) {
-		System.out.println("Hello Java");
+		double i, j, d;
+		i = 5;
+		j = 10;
+		
+		// Целью оператора if является блок
+		if (i != 0) {
+			System.out.println(i);
+			d = j / i;
+			System.out.println(d);
+		}
 	}
+
 }
