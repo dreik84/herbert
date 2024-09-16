cf8173532f5305d03824d878349805e1000adbc3
dreik84 <dreik84@mail.ru>
2024-09-16 16:03:57 +0300

    update Inches.java

---
+++ Inches.java
@@ -0,0 +1,14 @@
+/*
+ * Вычисляем количество кубических дюймов в кубической миле
+ */
+class Inches {
+	public static void main(String[] args) {
+		long ci;
+		long im;
+		
+		im = 5280 * 12;
+		ci = im * im * im;
+		
+		System.out.println("В кубической миле содержится " + ci + " кубических дюймов.");
+	}
+}
