8fce3d4babdf9333c98809db4083d7177ff0a3e7
dreik84 <dreik84@mail.ru>
2024-09-17 16:03:56 +0300

    update Hypot.java

---
+++ Hypot.java
@@ -0,0 +1,15 @@
+/*
+ * Теорема Пифагора для нахождения гипотенузы
+ */
+class Hypot {
+	public static void main(String[] args) {
+		double x, y, z;
+
+		x = 3;
+		y = 4;
+
+		z = Math.sqrt(x*x + y*y);
+
+		System.out.println("Длина гипотенузы: " + z);
+	}
+}
