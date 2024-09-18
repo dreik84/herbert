9df142254b99c27efd085af14366ca146dafdb46
dreik84 <dreik84@mail.ru>
2024-09-18 16:16:19 +0300

    create CharArithDemo.java

---
+++ CharArithDemo.java
@@ -0,0 +1,18 @@
+/*
+ * Демонстрация работы с типом char
+ */
+class CharArithDemo {
+	public static void main(String[] args) {
+		char ch;
+
+		ch = 'X';		
+		System.out.println("ch = " + ch);
+		
+		ch++;     // инкременирование ch
+		System.out.println("ch = " + ch);
+
+		ch = 90;  // присваивание ch значения Z
+		System.out.println("ch = " + ch);
+
+	}
+}
