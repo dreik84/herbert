81d1753d02779b671185d94680512b8cf8eb86fc
dreik84 <dreik84@mail.ru>
2024-10-07 16:14:15 +0300

    update Geuss.java

---
+++ Guess.java
@@ -0,0 +1,12 @@
+// Игра в угадывание буквы
+class Guess {
+	public static void main(String[] args) throws java.io.IOException {
+		char ch, answer = 'K';
+
+		System.out.println("Задумана буква между A и Z.");
+		System.out.print("Попробуйте её угадать: ");
+
+		ch = (char) System.in.read(); // чтение символа с клавиатуры
+		if (ch == answer) System.out.println(" ** Правильно ** ");
+	}
+}
