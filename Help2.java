24071cc985b97aeb3f839285b33f78e32de9c531
dreik84 <dreik84@mail.ru>
2024-10-20 16:15:57 +0300

    update Help2.java

--- Help2.java
+++ Help2.java
@@ -1,13 +1,24 @@
 // Справочная система по управляющим операторам Java
 class Help {
 	public static void main(String[] args) throws java.io.IOException {
-		char choice;
+		char choice, ignore;
 
-		System.out.println("Справка по: ");
-		System.out.println("  1. if");
-		System.out.println("  2. switch");
-		System.out.print("Выберите вариант: ");
-		choice = (char) System.in.read();
+		do {
+			System.out.println("Справка по: ");
+			System.out.println("  1. if");
+			System.out.println("  2. switch");
+			System.out.println("  3. for");
+			System.out.println("  4. while");
+			System.out.println("  5. do-while");
+			System.out.print("Выберите вариант: ");
+
+			choice = (char) System.in.read();
+
+			do {
+				ignore = (char) System.in.read();
+			} while (ignore != '\n');
+
+		} while (choice < '1' | choice > '5');
 
 		System.out.println("\n");
 
@@ -25,6 +36,21 @@ class Help {
 				System.out.println("    break;");
 				System.out.println("  // ...");
 				System.out.println("}");
+				break;
+			case '3':
+				System.out.println("Цикл for:\n");
+				System.out.println("for (инициализация; условие; итерация)");
+				System.out.println("	оператор;");
+				break;
+			case '4':
+				System.out.println("Цикл while:\n");
+				System.out.println("while (условие) оператор;");
+				break;
+			case '5':
+				System.out.println("Цикл do-while:\n");
+				System.out.println("do {");
+				System.out.println("	оператор;");
+				System.out.println("} while (условие);");
 				break;
 			default:
 				System.out.println("Выбранный вариант не найден.");
