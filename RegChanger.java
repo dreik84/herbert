0e343f850e2044433770b8a67a14f5b851e1f7f0
dreik84 <dreik84@mail.ru>
2024-10-29 16:10:28 +0300

    update RegChanger.java

--- RegChanger.java
+++ RegChanger.java
@@ -1,14 +1,22 @@
-// Программа ведёт подсчёт пробелов в введённом тексте
-class WSCounter {
+// Программа переводит символы в другой регистр
+class RegChanger {
 	public static void main(String[] args) throws java.io.IOException {
-		int count = 0;
+		var res = "";
 		char ch;
 
 		do {
 			ch = (char) System.in.read();
-			if (ch == ' ') count++;
+
+			if (ch <= 90 && ch >= 65) {
+				res += (char) (ch + 32);
+			} else if (ch <= 122 && ch >= 97) {
+                                res += (char) (ch - 32);
+                        } else {
+				res += ch;
+			}
+
 		} while ( ch != '.');
 
-		System.out.println("Количество пробелов: " + count);
+		System.out.println("Преобразованная строка: " + res);
 	}
 }
