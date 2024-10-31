4a30754d2055ef7b9d9966c64d26de3fe09f6c7a
dreik84 <dreik84@mail.ru>
2024-10-31 15:48:22 +0300

    update TwoVehicles.java

--- vehicle/TwoVehicles.java
+++ vehicle/TwoVehicles.java
@@ -1,18 +1,28 @@
 // В этом классе объявляется объект типа Vehicle
-class VehicleDemo {
+class TwoVehicles {
 	public static void main(String[] args) {
 		Vehicle minivan = new Vehicle();
-		int range;
+		Vehicle sportcar = new Vehicle();
+		int range1, range2;
 
-		// Присвоить значения полям объекта типа Vehicle
+		// Присвоить значения полям объекта minivan
 		minivan.passengers = 7;
 		minivan.fuelcap = 16;
 		minivan.mpg = 21;
 
-		// Рассчитать дальность при полном баке
-		range = minivan.fuelcap * minivan.mpg;
+		// Присвоить значения полям объекта sportcar
+                sportcar.passengers = 2;
+                sportcar.fuelcap = 14;
+                sportcar.mpg = 12;
+
+		// Рассчитать дальности при полном баке
+		range1 = minivan.fuelcap * minivan.mpg;
+		range2 = sportcar.fuelcap * sportcar.mpg;
 		
 		System.out.println("Минивэн может перевезти " + minivan.passengers +
-				" пассажиров на расстояние " + range + " миль.");
+				" пассажиров на расстояние " + range1 + " миль.");
+		System.out.println("Спортивный автомобиль может перевезти " + 
+				sportcar.passengers +
+                                " пассажиров на расстояние " + range2 + " миль.");
 	}
 }
