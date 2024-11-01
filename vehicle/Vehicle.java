969a5df177358b9dd20647dc43c067a8ac0bd6bd
dreik84 <dreik84@mail.ru>
2024-11-01 16:23:09 +0300

    update Vehicle.java

--- vehicle/Vehicle.java
+++ vehicle/Vehicle.java
@@ -3,4 +3,9 @@ class Vehicle {
 	int passengers; // количество пассажиров
 	int fuelcap;    // запас топлива в галлонах
 	int mpg;        // расход топлива в милях на галлон
+	
+	// Отображает дальность поездки
+	void range() {
+		System.out.println("Дальность поездки в милях: " + fuelcap * mpg);
+	}
 }
