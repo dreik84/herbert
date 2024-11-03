f4560ddcfc25e1db6f7501b9b108e64f7a7d2886
dreik84 <dreik84@mail.ru>
2024-11-03 16:37:07 +0300

    update ChkNum.java

---
+++ checknum/ChkNum.java
@@ -0,0 +1,8 @@
+// Простой пример использования параметра
+class ChkNum {
+	// Возвращает true, если значение x четное
+	boolean isEven(int x) {
+		if ((x % 2) == 0) return true;
+		else return false;
+	}
+}
