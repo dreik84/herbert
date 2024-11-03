7b9621e858ab30f20e49872db69284a3f2b93ee1
dreik84 <dreik84@mail.ru>
2024-11-03 16:42:38 +0300

    update ParmDemo.java

---
+++ checknum/ParmDemo.java
@@ -0,0 +1,9 @@
+class ParmDemo {
+	public static void main(String[] args) {
+		ChkNum e = new ChkNum();
+
+		if (e.isEven(10)) System.out.println("10 - четное.");
+		if (e.isEven(9)) System.out.println("9 - четное.");
+		if (e.isEven(8)) System.out.println("8 - четное.");
+	}
+}
