4190bcb9a096a99370aff7a5c22f3915bf5f2e0e
dreik84 <dreik84@mail.ru>
2024-11-09 15:55:47 +0300

    update ArrayDemo.java

---
+++ ArrayDemo.java
@@ -0,0 +1,12 @@
+// Демонстрация использования одномерного массива
+class ArrayDemo {
+	public static void main(String[] args) {
+		int[] sample = new int[10];
+		int i;
+
+		for (i = 0; i < 10; i++) sample[i] = i;
+		
+		for (i = 0; i < 10; i++)
+			System.out.println("Элемент sample[" + i + "]: " + sample[i]);
+	}
+}
