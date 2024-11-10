1b2e426d16673bd652fc5cfd0b3e026bf6051b31
dreik84 <dreik84@mail.ru>
2024-11-10 16:09:29 +0300

    update MinMax2.java

--- MinMax2.java
+++ MinMax2.java
@@ -1,20 +1,8 @@
-// Поиск минимального и максимального значений в массиве
-class MinMax {
+// Использование инициализатора массива
+class MinMax2 {
 	public static void main(String[] args) {
-		int[] nums = new int[10];
-		int min, max;
-
-		nums[0] = 99;
-		nums[1] = -10;
-		nums[2] = 100123;
-		nums[3] = 18;
-		nums[4] = -978;
-		nums[5] = 5623;
-		nums[6] = 463;
-		nums[7] = -9;
-		nums[8] = 287;
-		nums[9] = 49;
-
+		int[] nums = { 99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49 };
+		int min, max;		
 		min = max = nums[0];
 
 		for (int i = 0; i < 10; i++) {
