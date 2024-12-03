4b144834b84ab6c3f07f5f72cbb01dafa45974ab
dreik84 <dreik84@mail.ru>
2024-12-03 15:44:44 +0300

    update UpCase.java

---
+++ UpCase.java
@@ -0,0 +1,17 @@
+// Буквы верхнего регистра
+class UpCase {
+	public static void main(String[] args) {
+		char ch;
+
+		for (int i = 0; i < 10; i++) {
+			ch = (char) ('a' + i);
+			System.out.print(ch);
+
+			// Этот оператор отключает бит 6
+			ch = (char) ((int) ch & 65503); // теперь ch содержит букву 
+							// верхнего регистра
+			System.out.print(ch + " ");
+		}
+		System.out.println();
+	}
+}
