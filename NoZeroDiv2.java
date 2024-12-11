4437ef01f9173f26a46924b9fad4e84999e57ea0
dreik84 <dreik84@mail.ru>
2024-12-11 15:55:57 +0300

    update NoZeroDiv2.java

--- NoZeroDiv2.java
+++ NoZeroDiv2.java
@@ -1,13 +1,11 @@
 // Предотвращает деление на 0 с использованием операции ?
-class NoZeroDiv {
+class NoZeroDiv2 {
 	public static void main(String[] args) {
-		int result;
-
+		
 		for (int i = -5; i < 6; i++) {
-			result = i != 0 ? 100 / i : 0; // Предотвращает деление на 0
-
-			if (i != 0)
-				System.out.println("100 / " + i + " равно " + result);
+			
+			if (i != 0 ? true : false)
+				System.out.println("100 / " + i + " равно " + 100 / i);
 		}
 	}
 }
