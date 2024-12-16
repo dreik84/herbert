43bbcb005feb2a2a948567804f8b656a91f5af95
dreik84 <dreik84@mail.ru>
2024-12-16 15:55:33 +0300

    update FSDemo.java

---
+++ fsarrays/FSDemo.java
@@ -0,0 +1,31 @@
+// Демонстрация работы "отказоустойчивого" массива
+class FSDemo {
+	public static void main(String[] args) {
+		FailSoftArray fs = new FailSoftArray(5, -1);
+		int x;
+
+		// Отобразить отказы без учета
+		System.out.println("Обработка ошибок без отчета");
+		for (int i = 0; i < (fs.length * 2); i++) fs.put(i, i * 10);
+		
+		for (int i = 0; i < (fs.length * 2); i++) {
+			x = fs.get(i);
+			if (x != -1 ) System.out.print(x + " ");
+		}
+
+		System.out.println();
+
+		// Отработать отказы
+                System.out.println("\nОбработка ошибок с отчетом");
+                for (int i = 0; i < (fs.length * 2); i++)
+			if (!fs.put(i, i * 10))
+				System.out.println("Индекс " + i + "выходит за границы массива");
+		System.out.println();
+
+		for (int i = 0; i < (fs.length * 2); i++) {
+                        x = fs.get(i);
+                        if (x != -1 ) System.out.print(x + " ");
+			else System.out.println("Индекс " + i + "выходит за границы массива");
+		}
+	}
+}
