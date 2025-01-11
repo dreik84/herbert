8f26a78926c987c2997b12f04b0aef4cde314262
dreik84 <dreik84@mail.ru>
2025-01-11 15:53:35 +0300

    update VarArgs.java

---
+++ VarArgs.java
@@ -0,0 +1,22 @@
+// Демонстрация использования аргумента переменной длины
+class VarArgs {
+	
+	// Метод vaTest() принимает аргументы переменной длины
+	static void vaTest(int ... v) {
+		System.out.println("Количество аргуметов: " + v.length);
+		System.out.println("Содержимое: ");
+
+		for (int i = 0; i < v.length; i++)
+			System.out.println(" аргумент " + i + ": " + v[i]);
+
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		// Обратите внимание на то, что метод vaTest() можно вызывать
+		// с переменным количеством аргументов 
+		vaTest(10);        // один аргумент
+		vaTest(1, 2, 3);   // три аргумента
+		vaTest();          // без аргументов
+	}
+}
