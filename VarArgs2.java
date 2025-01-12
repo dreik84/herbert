c1ed0f1ec583ae9b5df56ad6e88b237e828406ce
dreik84 <dreik84@mail.ru>
2025-01-12 15:34:38 +0300

    update VarArgs2.java

---
+++ VarArgs2.java
@@ -0,0 +1,21 @@
+// Использование аргумента переменной длины вместе со стандартными аргументами
+class VarArgs2 {
+	
+	// Здесь msg - нормальный параметр, а v - параметр переменной длины
+	static void vaTest(String msg, int ... v) {
+		System.out.println(msg + v.length);
+		System.out.println("Содержимое: ");
+
+		for (int i = 0; i < v.length; i++) {
+			System.out.println("аргумент " + i + ": " + v[i]);
+
+		System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		vaTest("Один аргумент переменной длины: ", 10);
+		vaTest("Три аргумента переменной длины: ", 1, 2, 3);
+		vaTest("Без аргументов переменной длины: ");
+	}
+}
