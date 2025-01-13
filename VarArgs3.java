ee47c422b3b1055a6451b67be81a21d2f675cfa3
dreik84 <dreik84@mail.ru>
2025-01-13 16:02:28 +0300

    update VarArgs3.java

---
+++ VarArgs3.java
@@ -0,0 +1,37 @@
+// Перегрузка метода с аргументом переменной длины
+class VarArgs3 {
+	
+	static void vaTest(int ... v) {  // Первая версия vaTest()
+		System.out.println("vaTest(int ...): " + 
+				"Количество аргументов: " + v.length);
+		System.out.println("Содержимое: ");
+
+		for (int i = 0; i < v.length; i++)
+			System.out.println(" аргумент " + i + ": " + v[i]);
+		System.out.println();
+	}
+
+	static void vaTest(boolean ... v) {  // Вторая версия vaTest()
+                System.out.println("vaTest(boolean ...): " +
+                                "Количество аргументов: " + v.length);
+                System.out.println("Содержимое: ");
+
+                for (int i = 0; i < v.length; i++)
+                        System.out.println(" аргумент " + i + ": " + v[i]);
+                System.out.println();                                                          }
+
+	static void vaTest(String msg, int ... v) {  // Третья версия vaTest()
+                System.out.println("vaTest(String, int ...): " +
+                                msg + v.length);
+                System.out.println("Содержимое: ");
+
+                for (int i = 0; i < v.length; i++)
+                        System.out.println(" аргумент " + i + ": " + v[i]);
+                System.out.println();                                                          }
+
+	public static void main(String[] args) {
+		vaTest(1, 2, 3);
+		vaTest("Тестирование: ", 10, 20);
+		vaTest(true, false, false);
+	}
+}
