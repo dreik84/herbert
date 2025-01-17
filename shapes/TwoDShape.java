9339031632bd07a9b4b90072f0a9d36497ca32fd
dreik84 <dreik84@mail.ru>
2025-01-17 15:44:31 +0300

    update TwoDShape.java

---
+++ shapes/TwoDShape.java
@@ -0,0 +1,9 @@
+// Класс для представления двумерных объектов
+class TwoDShape {
+	double width;
+	double height;
+
+	void showDim() {
+		System.out.println("Ширина и высота: " + width + " и " + height);
+	}
+}
