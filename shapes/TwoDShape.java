568ec1f8830fe8d7a6faf1c05067d117b7caad1d
dreik84 <dreik84@mail.ru>
2025-01-20 16:01:37 +0300

    update TwoDShape.java

--- shapes/TwoDShape.java
+++ shapes/TwoDShape.java
@@ -1,7 +1,13 @@
 // Класс для представления двумерных объектов
 class TwoDShape {
-	double width;
-	double height;
+	private double width;   // теперь это
+	private double height;  // закрытые члены
+	
+	// Методы доступа для width и height
+	double getWidth() { return width; }
+	double getHeight() { return height; }
+	void setWidth(double w) { width = w; }
+	void setHeight(double h) { height = h; }
 
 	void showDim() {
 		System.out.println("Ширина и высота: " + width + " и " + height);
