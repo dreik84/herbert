b2e181d48b7549b2623aa8180ef733c11351d3b8
dreik84 <dreik84@mail.ru>
2025-01-21 15:47:51 +0300

    update Triangle.java

--- shapes/Triangle.java
+++ shapes/Triangle.java
@@ -1,6 +1,13 @@
 // Подкласс TwoDShape для представления треугольников
 class Triangle extends TwoDShape {
-	String style;
+	private String style;
+
+	// Конструктор
+	Triangle(String s, double w, double h) {
+		setWidth(w);
+		setHeight(h);
+		style = s;
+	}
 
 	double area() {
 		return getWidth() * getHeight() / 2; // доступ через геттеры
