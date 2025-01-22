2cc0b8ad1ec024dfb0e91d7a9a2916abc51d8bc0
dreik84 <dreik84@mail.ru>
2025-01-22 15:48:13 +0300

    update TwoDShapes.java

--- shapes/TwoDShape.java
+++ shapes/TwoDShape.java
@@ -3,6 +3,12 @@ class TwoDShape {
 	private double width;   // теперь это
 	private double height;  // закрытые члены
 	
+	// Параметризованный конструктор
+	TwoDShape(double w, double h) {
+		width = w;
+		height = h;
+	}
+	
 	// Методы доступа для width и height
 	double getWidth() { return width; }
 	double getHeight() { return height; }
