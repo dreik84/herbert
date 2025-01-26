d5192b1ad50e2d973fa023fb6aa4f215a38f1f5e
dreik84 <dreik84@mail.ru>
2025-01-26 16:21:18 +0300

    update TruckDemo.java

--- vehicle/TruckDemo.java
+++ vehicle/TruckDemo.java
@@ -1,18 +1,25 @@
-// В этом классе объявляется объект типа Vehicle
-class VehicleDemo {
+class TruckDemo {
 	public static void main(String[] args) {
-		Vehicle minivan = new Vehicle();
-		int range;
-
-		// Присвоить значения полям объекта типа Vehicle
-		minivan.passengers = 7;
-		minivan.fuelcap = 16;
-		minivan.mpg = 21;
-
-		// Рассчитать дальность при полном баке
-		range = minivan.fuelcap * minivan.mpg;
 		
-		System.out.println("Минивэн может перевезти " + minivan.passengers +
-				" пассажиров на расстояние " + range + " миль.");
+		// Сконструировать несколько объектов, представляющих грузовики
+		Truck semi = new Truck(2, 200, 7, 44000);
+		Truck pickup = new Truck(3, 28, 15, 2000);
+
+		double gallons;
+		int dist = 252;
+
+		gallons = semi.fuelNeeded(dist);
+		System.out.println("Полуприцеп может перевезти " +
+					semi.getCargo() + " фунтов");
+		System.out.println("Для поездки на расстояние " + dist +
+					" миль полуприцепу требуется " + 
+					gallons + " галлонов топлива");
+
+		gallons = pickup.fuelNeeded(dist);
+                System.out.println("Пикап может перевезти " +
+                                        pickup.getCargo() + " фунтов");
+                System.out.println("Для поездки на расстояние " + dist +
+                                        " миль пикапу требуется " +
+                                        gallons + " галлонов топлива");
 	}
 }
