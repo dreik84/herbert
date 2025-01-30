f308e7420bd5fade4252f07eb3b77ea68271ac05
dreik84 <dreik84@mail.ru>
2025-01-30 16:03:37 +0300

    update Shapes7.java

--- shapes/Shapes7.java
+++ shapes/Shapes7.java
@@ -1,19 +1,19 @@
-class Shape6 {
+class Shapes7 {
 	public static void main(String[] args) {
-		ColorTriangle t1 = new ColorTriangle("синий","контурный", 8.0, 12.0);
-		ColorTriangle t2 = new ColorTriangle("красный","закрашеный", 4.0, 4.0);
+		Triangle t1 = new Triangle("контурный", 8.0, 12.0);
+
+		// Создать копию объекта t1
+		Triangle t2 = new Triangle(t1);
 		
 		System.out.println("Информация об объекте t1: ");
 		t1.showStyle();
 		t1.showDim();
-		t1.showColor();
 		System.out.println("Площадь: " + t1.area());
 		System.out.println();
 	
 		System.out.println("Информация об объекте t2: ");
                 t2.showStyle();
                 t2.showDim();
-		t2.showColor();
                 System.out.println("Площадь: " + t2.area());
                 System.out.println();
 	}
