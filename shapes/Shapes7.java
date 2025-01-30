80197dec1bf9843ba93b8aae1ed7f2e621f69ba6
dreik84 <dreik84@mail.ru>
2025-01-30 15:58:57 +0300

    create Shapes7.java

---
+++ shapes/Shapes7.java
@@ -0,0 +1,20 @@
+class Shape6 {
+	public static void main(String[] args) {
+		ColorTriangle t1 = new ColorTriangle("синий","контурный", 8.0, 12.0);
+		ColorTriangle t2 = new ColorTriangle("красный","закрашеный", 4.0, 4.0);
+		
+		System.out.println("Информация об объекте t1: ");
+		t1.showStyle();
+		t1.showDim();
+		t1.showColor();
+		System.out.println("Площадь: " + t1.area());
+		System.out.println();
+	
+		System.out.println("Информация об объекте t2: ");
+                t2.showStyle();
+                t2.showDim();
+		t2.showColor();
+                System.out.println("Площадь: " + t2.area());
+                System.out.println();
+	}
+}
