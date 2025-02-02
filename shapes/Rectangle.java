6716f895c2362f372172d9bd826f9ad651bae1ae
dreik84 <dreik84@mail.ru>
2025-02-02 16:06:03 +0300

    update Rectangle.java

--- shapes/Rectangle.java
+++ shapes/Rectangle.java
@@ -1,12 +1,33 @@
 // Подсласс TwoDShape для представления прямоугольников
 class Rectangle extends TwoDShape {
-	
-	boolean isSquare() {
-		if (width == height) return true;
-		return false;
+
+	// Стандартный конструктор
+	Rectangle() {
+		super();
 	}
 
+	// Конструктор класса Rectangle
+	Rectangle(double w, double h) {
+		super(w, h, "прямоугольник");
+	}
+
+	// Конструктор квадрата
+	Rectangle(double x) {
+		super(x, "прямоугольник");
+	}
+
+	// Конструктор объекта из объкта
+	Rectangle(Rectangle ob) {
+		super(ob);
+	}
+	
+	boolean isSquare() {
+		if (getWidth() == getHeight()) return true;
+		return false;
+	}
+	
+	// Переопределение метода area() для Rectangle
 	double area() {
-		return width * height;
+		return getWidth() * getHeight();
 	}
 }
