2dec70be19b7d058cef5d0cb81428981613530e6
dreik84 <dreik84@mail.ru>
2025-02-04 15:42:56 +0300

    update FinalD.java

--- FinalD.java
+++ FinalD.java
@@ -1,15 +1,22 @@
-class ErrMsg {
+class FinalD {
 	public static void main(String[] args) {
 		ErrorMsg err = new ErrorMsg();
 
-		System.out.println(err.getErrorMsg(2));
-		System.out.println(err.getErrorMsg(19));
+		System.out.println(err.getErrorMsg(err.OUTERR));
+		System.out.println(err.getErrorMsg(err.DISKERR));
 	}
 }
 
 
 // Возвращение объекта типа String
 class ErrorMsg {
+
+	// Коды ошибок
+	final int OUTERR = 0;
+	final int INERR = 1;
+	final int DISKERR = 2;
+	final int INDEXERR = 3;
+
 	String[] msgs = {
 		"Ошибка вывода",
 		"Ошибка ввода",
