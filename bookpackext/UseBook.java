28d3c7c1b4af7fbc9282acf3da1d8bf0c3421880
dreik84 <dreik84@mail.ru>
2025-02-10 15:47:52 +0300

    update UseBook.java

--- bookpackext/UseBook.java
+++ bookpackext/UseBook.java
@@ -1,15 +1,17 @@
 package bookpackext;
 
+import bookpack.*; // Импортирование пакета bookpack
+
 // Использование класса Book из пакета bookpack
 class UseBook {
 	public static void main(String[] args) {
-		bookpack.Book[] books = new bookpack.Book[5];
+		Book[] books = new Book[5];
 
-		books[0] = new bookpack.Book("Java: A Beginner's Guide", "Schildt", 2022);
-		books[1] = new bookpack.Book("Java: The Complete Reference", "Schildt", 2022);
-		books[2] = new bookpack.Book("1984", "Orwell", 1949);
-		books[3] = new bookpack.Book("Red Storm Rising", "Clancy", 1986);
-		books[4] = new bookpack.Book("On the Road", "Kerouac", 1955);
+		books[0] = new Book("Java: A Beginner's Guide", "Schildt", 2022);
+		books[1] = new Book("Java: The Complete Reference", "Schildt", 2022);
+		books[2] = new Book("1984", "Orwell", 1949);
+		books[3] = new Book("Red Storm Rising", "Clancy", 1986);
+		books[4] = new Book("On the Road", "Kerouac", 1955);
 
 		for (int i = 0; i < books.length; i++) {
 			books[i].show();
