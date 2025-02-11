7e1651a649f6f4d4cdb2e8f2b479e70cbe61e635
dreik84 <dreik84@mail.ru>
2025-02-11 15:57:13 +0300

    update ByTwos.java

---
+++ ByTwos.java
@@ -0,0 +1,24 @@
+// Реализация интерфейса Series
+class ByTwos implements Series {
+	int start;
+	int val;
+
+	ByTwos() {
+		start = 0;
+		val = 0;
+	}
+
+	public int genNext() {
+		val += 2;
+		return val;
+	}
+
+	public void reset() {
+		val = start;
+	}
+
+	public void setStart(int x) {
+		start = x;		
+		val = x;
+	}
+}
