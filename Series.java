14663caa09bd24e33538d5b141f192866b37816b
dreik84 <dreik84@mail.ru>
2025-02-11 15:50:41 +0300

    update Series.java

---
+++ Series.java
@@ -0,0 +1,5 @@
+public interface Series {
+	int getNext();         // возврат следующего числа в последовательности
+	void reset();          // сброс
+	void setStart(int x);  // установка начального значения
+}
