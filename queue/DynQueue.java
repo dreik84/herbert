9f6a0e8af1810cece87919255d6020bf57c7c8af
dreik84 <dreik84@mail.ru>
2025-02-14 16:25:34 +0300

    update DynQueue.java

--- queue/DynQueue.java
+++ queue/DynQueue.java
@@ -1,22 +1,26 @@
-// Кольцевая очередь
-class CircularQueue implements ICharQ {
+// Динамическая очередь
+class DynQueue implements ICharQ {
 	private char[] q;           // массив, в котором хранится очередь
 	private int putloc, getloc; // индексы для помещения и извлечения
 	
 	// Конструктор пустой очереди заданного размера
-	public CircularQueue(int size) {
-		q = new char[size + 1];  // выделение памяти под очередь
+	public DynQueue(int size) {
+		q = new char[size];  // выделение памяти под очередь
 		putloc = getloc = 0;
 	}
 
 	// Поместить символ в очередь
 	public void put(char ch) {
-		if (putloc + 1 == getloc | ((putloc == q.length - 1) & (getloc == 0))) {
-			System.out.println("Очередь переполнена");
-			return;
+		if (putloc == q.length) {
+			// Увеличить размер очереди
+			char[] t = new char[q.length * 2];
+
+			// Скопировать элементы в новую очередь
+			for (int i = 0; i < q.length; i++) t[i] = q[i];
+
+			q = t;
 		}
 		q[putloc++] = ch;
-		if (putloc == q.length) putloc = 0; // закольцевать	
 	}
 
 	// Извлечь символ из очереди
@@ -25,8 +29,7 @@ class CircularQueue implements ICharQ {
 			System.out.println("Очередь пуста");
 			return (char) 0;
 		}
-		char ch = q[getloc++];
-		if (getloc == q.length) getloc = 0; // закольцевать
-		return ch;
-	}
+		return q[getloc++];
+	}	
+
 }
