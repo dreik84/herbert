f88b93799d2bbd2a0494e36ee447bbd688c852bc
dreik84 <dreik84@mail.ru>
2025-02-20 15:54:23 +0300

    update Series.java

--- Series.java
+++ Series.java
@@ -1,5 +1,15 @@
 public interface Series {
 	int getNext();         // возврат следующего числа в последовательности
+	
+	// Возвращает массис, содержащий следующие n элементов
+	default int[] getNextArray(int n) {
+		int[] vals = new int[n];
+
+		for (int i = 0; i < n; i++) vals[i] = getNext();
+
+		return vals;
+	}
+
 	void reset();          // сброс
 	void setStart(int x);  // установка начального значения
 }
