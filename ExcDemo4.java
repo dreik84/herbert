4285c3d85ccbca70401e5dba080c1453f2745cd5
dreik84 <dreik84@mail.ru>
2025-02-24 15:55:12 +0300

    update ExcDemo4.java

--- ExcDemo4.java
+++ ExcDemo4.java
@@ -1,16 +1,19 @@
-// Изящно обработать ошибку и продолжить работу
-class ExcDemo3 {
+// Использование нескольких операторов catch
+class ExcDemo4 {
 	public static void main(String[] args) {
-		int[] numer = { 4, 8, 16, 32, 64, 128 };
-		int[] denum = { 2, 0, 4, 4, 0, 8 };
+		int[] numer = { 4, 8, 16, 32, 64, 128, 256, 512 };
+		int[] denom = { 2, 0, 4, 4, 0, 8 };
 		
 		for (int i = 0; i < numer.length; i++) {
 			try {
-				System.out.println(numer[i] + " / " + denum[i] + " равно " +
-						numer[i]/denum[i]);
+				System.out.println(numer[i] + " / " + denom[i] + " равно " +
+						numer[i]/denom[i]);
 			} catch (ArithmeticException exc) {
 				// Перехватить исключение
 				System.out.println("Деление на ноль невозможно");
+			} catch (ArrayIndexOutOfBoundsException exc) {
+				// Перехватить исключение
+                                System.out.println("Соответствующий элемент не найден");
 			}
 		}
 	}
