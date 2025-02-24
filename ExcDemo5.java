5eee4ba5aea604babb734e0f38c5ebfb8c7e05cd
dreik84 <dreik84@mail.ru>
2025-02-24 16:02:06 +0300

    update ExcDemo5.java

--- ExcDemo5.java
+++ ExcDemo5.java
@@ -1,19 +1,19 @@
 // Использование нескольких операторов catch
-class ExcDemo4 {
+class ExcDemo5 {
 	public static void main(String[] args) {
 		int[] numer = { 4, 8, 16, 32, 64, 128, 256, 512 };
 		int[] denom = { 2, 0, 4, 4, 0, 8 };
-		
+	
 		for (int i = 0; i < numer.length; i++) {
 			try {
 				System.out.println(numer[i] + " / " + denom[i] + " равно " +
 						numer[i]/denom[i]);
-			} catch (ArithmeticException exc) {
-				// Перехватить исключение
-				System.out.println("Деление на ноль невозможно");
 			} catch (ArrayIndexOutOfBoundsException exc) {
-				// Перехватить исключение
-                                System.out.println("Соответствующий элемент не найден");
+				// Перехватить исключение подкласса
+				System.out.println("Соответствующий элемент не найден");
+			} catch (Throwable exc) {
+				// Перехватить исключение суперкласса
+                                System.out.println("Возникло какое-то исключение");
 			}
 		}
 	}
