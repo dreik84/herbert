6540bf7b6a280e700bacc3d2e3a40d9cd74a8236
dreik84 <dreik84@mail.ru>
2025-02-25 15:45:49 +0300

    update NestTrys.java

--- NestTrys.java
+++ NestTrys.java
@@ -1,20 +1,25 @@
-// Использование нескольких операторов catch
-class ExcDemo5 {
+// Использование вложенного блока try
+class NestTrys {
 	public static void main(String[] args) {
 		int[] numer = { 4, 8, 16, 32, 64, 128, 256, 512 };
 		int[] denom = { 2, 0, 4, 4, 0, 8 };
-	
-		for (int i = 0; i < numer.length; i++) {
-			try {
-				System.out.println(numer[i] + " / " + denom[i] + " равно " +
-						numer[i]/denom[i]);
-			} catch (ArrayIndexOutOfBoundsException exc) {
-				// Перехватить исключение подкласса
-				System.out.println("Соответствующий элемент не найден");
-			} catch (Throwable exc) {
-				// Перехватить исключение суперкласса
-                                System.out.println("Возникло какое-то исключение");
-			}
+
+		try {
+			// Внешний блок try
+			for (int i = 0; i < numer.length; i++) {
+                        try {
+				// Вложенный блок tryA
+                                System.out.println(numer[i] + " / " + denom[i] + " равно " +
+                                                numer[i]/denom[i]);
+                        } catch (ArithmeticException exc) {
+                                // Перехватить исключение
+                                System.out.println("Деление на ноль невозможно");
+                        }
+                }
+		} catch (ArrayIndexOutOfBoundsException exc) {
+                                // Перехватить исключение
+                                System.out.println("Соответствующий элемент не найден");
+                                System.out.println("Неисправимая ошибка - программа завершена");
 		}
 	}
 }
