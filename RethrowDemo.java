383ecbe02eec23a1589555e63b239fbb54cc5f50
dreik84 <dreik84@mail.ru>
2025-02-25 16:14:24 +0300

    update RethrowDemo.java

--- RethrowDemo.java
+++ RethrowDemo.java
@@ -1,6 +1,17 @@
-// Использование нескольких операторов catch
-class ExcDemo5 {
+class RethrowDemo {
 	public static void main(String[] args) {
+		try {
+			Rethrow.genException();
+		} catch (ArrayIndexOutOfBoundsException exc) {
+			// Повторно перехватить исключение
+			System.out.println("Неисправимая ошибка - программа завершена");
+		}
+	}
+}
+
+// Повторная генерация исключений
+class Rethrow {
+	public static void genException() {
 		int[] numer = { 4, 8, 16, 32, 64, 128, 256, 512 };
 		int[] denom = { 2, 0, 4, 4, 0, 8 };
 	
@@ -8,13 +19,16 @@ class ExcDemo5 {
 			try {
 				System.out.println(numer[i] + " / " + denom[i] + " равно " +
 						numer[i]/denom[i]);
-			} catch (ArrayIndexOutOfBoundsException exc) {
-				// Перехватить исключение подкласса
-				System.out.println("Соответствующий элемент не найден");
-			} catch (Throwable exc) {
-				// Перехватить исключение суперкласса
-                                System.out.println("Возникло какое-то исключение");
+			} catch (ArithmeticException exc) {
+				// Перехватить исключение
+				System.out.println("Деление на ноль невозможно");
+
+		 	} catch (ArrayIndexOutOfBoundsException exc) {
+                                // Перехватить исключение
+                                System.out.println("Соответствующий элемент не найден");
+				throw exc; // повторно сгенерировать исключение
 			}
 		}
 	}
 }
+
