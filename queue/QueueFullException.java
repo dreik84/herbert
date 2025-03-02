3ce2e0029660c47d09e6f73b7b1c5e7949b5c0ff
dreik84 <dreik84@mail.ru>
2025-03-02 15:46:49 +0300

    update QueueFullException.java

---
+++ queue/QueueFullException.java
@@ -0,0 +1,13 @@
+// Исключение для ошибок, связанных с переполненной очередью
+public class QueueFullException extends Exception {
+	int size;
+
+	QueueFullException(int s) {
+		size = s;
+	}
+
+	public String toString() {
+		return "\nОчередь переполнена. Максимальный размер составляет "
+			+ size + " элементов.";
+	}
+}
