328c6b7a35a4b9c5cd5a0f135a5b6a8740b4d74a
dreik84 <dreik84@mail.ru>
2025-03-16 16:34:47 +0300

    update MyThread.java

--- threads/MyThread.java
+++ threads/MyThread.java
@@ -1,24 +1,33 @@
 // Создание потока путем реализации интерфейса Runnable
 class MyThread implements Runnable {
-	String thrdName;
-
+	Thread thrd; // ссылка на поток
+	
+	// Сконструировать новый поток
 	MyThread(String name) {
-		thrdName = name;
+		thrd = new Thread(this, name); // При создании потоку назначается имя
+	}
+
+	// Фабричный метод, который создает и запускает поток
+	public static MyThread createAndStart(String name) {
+		MyThread myThrd = new MyThread(name);
+
+		myThrd.thrd.start(); // запустить поток
+		return myThrd;
 	}
 
 	// Точка входа в поток
 	public void run() {
-		System.out.println("Поток " + thrdName + "запущен");
+		System.out.println("Поток " + thrd.getName() + "запущен");
 		
 		try {
 			for (int count = 0; count < 10; count++) {
 				Thread.sleep(400);
-				System.out.println("В потоке " + thrdName +
+				System.out.println("В потоке " + thrd.getName() +
 					"значение count равно " + count);
 			}
 		} catch (InterruptedException exc) {
-			System.out.println("Поток " + thrdName + "прерван");
+			System.out.println("Поток " + thrd.getName() + "прерван");
 		}
-		System.out.println("Поток " + thrdName + "завершен");
+		System.out.println("Поток " + thrd.getName() + "завершен");
 	}
 }
