c8befb148f6c632c7a663a93bee09a0c72230887
dreik84 <dreik84@mail.ru>
2025-03-18 15:52:55 +0300

    update MoreThreads.java

--- threads/MoreThreads.java
+++ threads/MoreThreads.java
@@ -7,16 +7,19 @@ class MoreThreads {
 		MyThread mt2 = MyThread.createAndStart("Child #2");
 		MyThread mt3 = MyThread.createAndStart("Child #3");
 
-		for (int i = 0; i < 50; i++) {
+		do {
 			System.out.print(".");
 
-			try {
-				Thread.sleep(100);
-			}
-			catch (InterruptedException exc) {
-				System.out.println("Главный поток прерван");
-			}
-		}
+                        try {
+                                Thread.sleep(100);
+                        }
+                        catch (InterruptedException exc) {
+                                System.out.println("Главный поток прерван");
+                        }
+		} while (mt1.thrd.isAlive() || 
+			 mt2.thrd.isAlive() || 
+			 mt3.thrd.isAlive());  // Пока не завершатся все потоки
+
 		System.out.println("Главный поток завершен");
 	}
 }
