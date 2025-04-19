7f395d8b53d47101ed49e68db0f5aaee7727a2be
dreik84 <dreik84@mail.ru>
2025-04-19 16:15:05 +0300

    update module-info.java to appfuncs

--- mymodapp/appsrc/appfuncs/module-info.java
+++ mymodapp/appsrc/appfuncs/module-info.java
@@ -1,5 +1,8 @@
 // Определение модуля математических функций
 module appfuncs {
 	// Экспортировать пакет
-	exports appfuncs.simplefuncs to appstart;
+	exports appfuncs.simplefuncs;
+
+	// Сделать модуль appsupport транзитивным
+	requires transitive appsupport;
 }
