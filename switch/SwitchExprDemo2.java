efbc3811fc1778d4576cd61ddced1135c6bc761b
dreik84 <dreik84@mail.ru>
2025-04-26 16:08:33 +0300

    update SwitchExprDemo2.java

--- switch/SwitchExprDemo2.java
+++ switch/SwitchExprDemo2.java
@@ -1,5 +1,5 @@
-// Преобразование оператора switch в выражение switch
-class SwitchExprDemo {
+// Использование оператора switch со стрелкой
+class SwitchExprDemo2 {
 	
 	enum ShipMethod { STANDARD, TRUCK, AIR, OVERNIGHT }
 
@@ -8,14 +8,10 @@ class SwitchExprDemo {
 		int productID = 5099;
 
 		ShipMethod shipBy =  switch (productID) {
-			case 1774, 8708, 6709:
-				yield ShipMethod.TRUCK;
-			case 4657, 2195, 3621, 1887:
-				yield ShipMethod.AIR;
-			case 2907, 5099:
-				yield ShipMethod.OVERNIGHT;
-			default:
-				yield ShipMethod.STANDARD;
+			case 1774, 8708, 6709 -> ShipMethod.TRUCK;
+			case 4657, 2195, 3621, 1887 -> ShipMethod.AIR;
+			case 2907, 5099 -> ShipMethod.OVERNIGHT;
+			default -> ShipMethod.STANDARD;
 		}; // точка с запятой здесь обязательна
 
 		System.out.println("Способ доставки товара с идентификатором " + 
