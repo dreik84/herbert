11101d23e866008c0aaca5d11c88658641e70b8a
dreik84 <dreik84@mail.ru>
2025-04-26 16:25:38 +0300

    update BlockArrowCaseDemo.java

--- switch/BlockArrowCaseDemo.java
+++ switch/BlockArrowCaseDemo.java
@@ -1,24 +1,35 @@
-// Преобразование оператора switch в выражение switch
-class SwitchExprDemo {
+// Использование блоков кода в операторах switch со стрелкой
+class BlockArrowCaseDemo {
 	
 	enum ShipMethod { STANDARD, TRUCK, AIR, OVERNIGHT }
 
 	public static void main(String[] args) {
 		
 		int productID = 5099;
+		boolean extraCharge;
 
 		ShipMethod shipBy =  switch (productID) {
-			case 1774, 8708, 6709:
+			case 1774, 8708, 6709 -> {
+				extraCharge = true;
 				yield ShipMethod.TRUCK;
-			case 4657, 2195, 3621, 1887:
+			}
+			case 4657, 2195, 3621, 1887 -> {
+				extraCharge = false;
 				yield ShipMethod.AIR;
-			case 2907, 5099:
+			}
+			case 2907, 5099 -> {
+				extraCharge = true;
 				yield ShipMethod.OVERNIGHT;
-			default:
+			}
+			default -> {
+				extraCharge = false;
 				yield ShipMethod.STANDARD;
+			}
 		}; // точка с запятой здесь обязательна
 
 		System.out.println("Способ доставки товара с идентификатором " + 
 				productID + ": " + shipBy);
+
+		if (extraCharge) System.out.println("Требуется доплата");
 	}
 }
