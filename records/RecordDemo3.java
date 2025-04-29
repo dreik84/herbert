322aa773a602b10c1aa8633d92509e42fce29ad5
dreik84 <dreik84@mail.ru>
2025-04-29 16:05:27 +0300

    update RecordDemo3.java

--- records/RecordDemo3.java
+++ records/RecordDemo3.java
@@ -1,7 +1,14 @@
-// Простой пример работы с записями
-record Item(String name, int itemNum, double price){}
+// Использование неканонического конструктора
+record Item(String name, int itemNum, double price) {
+	
+	static double pricePending = -1;
 
-class RecordDemo {
+	public Item(String name, int itemNum, String price) {
+		this(name, itemNum, Double.parseDouble(price));
+	}
+}
+
+class RecordDemo3 {
 	public static void main(String[] args) {
 		
 		// Создать массив записей
@@ -10,8 +17,8 @@ class RecordDemo {
 		// Заполнить массив элеметами
 		items[0] = new Item("Hammer", 257, 10.99);
 		items[1] = new Item("Wrench", 18, 19.29);
-		items[2] = new Item("Drill", 903, 22.25);
-		items[3] = new Item("Saw", 27, 34.59);
+		items[2] = new Item("Drill", 903, "22.25");
+		items[3] = new Item("Saw", 27, "34.59");
 
 		// Использовать методы доступа
 		for (Item myItem : items) {
