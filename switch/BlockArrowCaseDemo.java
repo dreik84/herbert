54fadc7ab7181308de3b321798f0cf51ea40be94
dreik84 <dreik84@mail.ru>
2025-04-30 16:32:04 +0300

    update BlockArrowCaseDemo.java

--- switch/BlockArrowCaseDemo.java
+++ switch/BlockArrowCaseDemo.java
@@ -2,34 +2,22 @@
 class BlockArrowCaseDemo {
 	
 	enum ShipMethod { STANDARD, TRUCK, AIR, OVERNIGHT }
+	record MyRec(boolean extraCharge, ShipMethod method){}
 
 	public static void main(String[] args) {
 		
 		int productID = 5099;
-		boolean extraCharge;
 
-		ShipMethod shipBy =  switch (productID) {
-			case 1774, 8708, 6709 -> {
-				extraCharge = true;
-				yield ShipMethod.TRUCK;
-			}
-			case 4657, 2195, 3621, 1887 -> {
-				extraCharge = false;
-				yield ShipMethod.AIR;
-			}
-			case 2907, 5099 -> {
-				extraCharge = true;
-				yield ShipMethod.OVERNIGHT;
-			}
-			default -> {
-				extraCharge = false;
-				yield ShipMethod.STANDARD;
-			}
+		MyRec shipBy =  switch (productID) {
+			case 1774, 8708, 6709 -> new MyRec(true, ShipMethod.TRUCK);
+			case 4657, 2195, 3621, 1887 -> new MyRec(true, ShipMethod.AIR);
+			case 2907, 5099 -> new MyRec(true, ShipMethod.OVERNIGHT);
+			default -> new MyRec(true, ShipMethod.STANDARD);
 		}; // точка с запятой здесь обязательна
 
 		System.out.println("Способ доставки товара с идентификатором " + 
 				productID + ": " + shipBy);
 
-		if (extraCharge) System.out.println("Требуется доплата");
+		if (shipBy.extraCharge()) System.out.println("Требуется доплата");
 	}
 }
