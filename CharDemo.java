1e304a7b780671a9b6faf685acd730abaca42b7b
dreik84 <dreik84@mail.ru>
2026-04-24 16:06:02 +0300

    Create CharDemo.java

---
+++ CharDemo.java
@@ -0,0 +1,14 @@
+// Демонстрация использования типа char
+class CharDemo {
+	public static void main(String[] args) {
+		char ch1, ch2, ch3;
+		
+		ch1 = 88; // X
+		ch2 = 'Y';
+		//ch3 = ch2 + 1;
+
+		System.out.println("ch1: " + ch1);
+		System.out.println("ch2: " + ch2);
+		System.out.println("ch3: " + ++ch2);
+	}	
+}
