19f3142570c9ad301f144c0d2e40b5b30406bae7
dreik84 <dreik84@mail.ru>
2026-05-06 16:32:16 +0300

    Create VarDemo.java

---
+++ VarDemo.java
@@ -0,0 +1,13 @@
+// Простая демонстрация выведения типов локальных переменных
+class VarDemo {
+	public static void main(String[] args) {
+		var avg = 10.0; // double
+		System.out.println("Значение avg: " + avg);
+
+		int var = 1;
+		System.out.println("Значение var: " + var);
+
+		var k = -var;
+		System.out.println("Значение k: " + k);
+	}
+}
