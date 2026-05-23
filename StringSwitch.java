ea9a51e0166744ab4c694079fcd2005076211978
dreik84 <dreik84@mail.ru>
2026-05-23 16:20:15 +0300

    Create StringSwitch.java

---
+++ StringSwitch.java
@@ -0,0 +1,22 @@
+// Применение строки для управления оператором switch
+class StringSwitch {
+	public static void main(String[] args) {
+		
+		String str = "two";
+
+		switch (str) {
+			case "one":
+				System.out.println("one");
+				break;
+			case "two":
+				System.out.println("two");
+				break;
+			case "three":
+				System.out.println("three");
+				break;
+			default:
+				System.out.println("none");
+				break;
+		}
+	}
+}
