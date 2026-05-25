40c0761cdb1c84538f69800e610b099f63a913e7
dreik84 <dreik84@mail.ru>
2026-05-25 16:08:46 +0300

    Create DoWhile.java

---
+++ DoWhile.java
@@ -0,0 +1,12 @@
+// Демонстрация работы цикла do-while
+class DoWhile {
+	public static void main(String[] args) {
+		
+		int n = 10;
+
+		do {
+			System.out.println(n--);
+
+		} while (n > 0);
+	}	
+}
