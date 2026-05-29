a6388997f8c62525c41ffc03e6aff51dbd4aa5b5
dreik84 <dreik84@mail.ru>
2026-05-29 16:05:47 +0300

    Create ForEach3.java

---
+++ ForEach3.java
@@ -0,0 +1,20 @@
+// Использование цикла for в стиле "for-each" для прохода по многомерному массиву
+class ForEach3 {
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		int[][] nums = new int[3][5];
+
+		for (int i = 0; i < 3; i++) 
+			for (int j = 0; j < 5; j++)
+				nums[i][j] = (i + 1) * (j + 1);
+
+		for (int[] x : nums)
+			for (int y : x) {
+				System.out.println(y);
+				sum += y;
+			}
+
+		System.out.println("sum : " + sum);
+	}
+}	
