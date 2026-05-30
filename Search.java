271bdb106ddaa87dcef73ec06bda5aa80af67800
dreik84 <dreik84@mail.ru>
2026-05-30 16:37:19 +0300

    Creaet Search.java

---
+++ Search.java
@@ -0,0 +1,20 @@
+// Поиск в массиве с использованием цикла for в стиле "for-each"
+class Search {
+	public static void main(String[] args) {
+		
+		int[] nums = { 6, 8, 3, 7, 5, 6, 1, 4 };
+		int val = 5;
+		boolean found = false;
+
+		for (int x : nums) {
+			
+			if (x == val) {
+				found = true;
+				break;
+			}
+		}
+
+		if (found)
+			System.out.println(found);
+	}
+}
