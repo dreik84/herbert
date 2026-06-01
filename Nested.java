16c720ad45e007957428c27c5e7eed7a05423bff
dreik84 <dreik84@mail.ru>
2026-06-01 16:06:28 +0300

    Create Nested.java

---
+++ Nested.java
@@ -0,0 +1,14 @@
+// Циклы могут быть вложенными
+class Nested {
+	public static void main(String[] args) {
+		
+		int i, j;
+
+		for (i = 0; i < 10; i++) {
+			for (j = i; j < 10; j++) 
+				System.out.print('.');
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
