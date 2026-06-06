8ad6392fadaa4a9345dad045b96e4144652fa2c5
dreik84 <dreik84@mail.ru>
2026-06-06 16:27:34 +0300

    Create BoxDemo.java

---
+++ BoxDemo.java
@@ -0,0 +1,22 @@
+// Программа, в которой используется класс Box
+class Box {
+	double width;
+	double height;
+	double depth;
+}
+
+class BoxDemo {
+	public static void main(String[] args) {
+		
+		Box mybox = new Box();
+		double vol;
+
+		mybox.width = 10;
+		mybox.height = 20;
+		mybox.depth = 15;
+
+		vol = mybox.width * mybox.height * mybox.depth; // объем коробки
+
+		System.out.println("Объем коробки равен " + vol);
+	}
+}
