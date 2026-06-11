99af1c9026697241b9d86b4cfd8ac207a839214d
dreik84 <dreik84@mail.ru>
2026-06-11 16:09:56 +0300

    Create BoxDemo4.java

---
+++ BoxDemo4.java
@@ -0,0 +1,30 @@
+// Добавляем метод в класс Box
+class BoxDemo3 {
+	public static void main(String[] args) {
+		
+		Box mybox1 = new Box();
+		Box mybox2 = new Box();
+		double vol;
+
+		mybox1.width = 10;
+		mybox1.height = 20;
+		mybox1.depth = 15;
+
+		mybox2.width = 3;
+		mybox2.height = 6;
+		mybox2.depth = 9;
+
+		System.out.println("Volume is " + mybox1.volume());
+		System.out.println("Volume is " + mybox2.volume());
+	}
+}
+
+class Box {
+	double width;
+	double height;
+	double depth;
+
+	double volume() {
+		return width * height * depth;
+	}
+}
