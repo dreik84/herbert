ef3aebbe686745731dc8c66f4fcd121a7a28b54a
dreik84 <dreik84@mail.ru>
2026-06-20 16:28:06 +0300

    Create PassObjRef.java

---
+++ PassObjRef.java
@@ -0,0 +1,29 @@
+// Объекты передаются через ссылки на них
+
+class PassObjRef {
+	public static void main(String[] args) {
+		
+		Test ob = new Test(15, 20);
+
+		System.out.println("ob.a и ob.b перед вызовом: " + ob.a + " " + ob.b);
+
+		ob.meth(ob);
+
+		System.out.println("ob.a и ob.b после вызова: " + ob.a + " " + ob.b);
+	}
+}
+
+class Test {
+	
+	int a, b;
+
+	Test (int i, int j) {
+		a = i;
+		b = j;
+	}
+
+	void meth(Test o) {
+		o.a *= 2;
+		o.b /= 2;
+	}
+}
