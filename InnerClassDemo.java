927df14232596a16bdb2f23fcb7238cae4f8abf2
dreik84 <dreik84@mail.ru>
2026-06-29 16:34:58 +0300

    Update InnerClassDemo.java

--- InnerClassDemo.java
+++ InnerClassDemo.java
@@ -13,16 +13,36 @@ class Outer {
 	int outerX = 100;
 	
 	void test() {
+
+		for (int i = 0; i < 10; i++) {
+			
+			class Inner2 {
+				void display() {
+					System.out.println("outerX = " + outerX);
+				}
+			}
+
+			Inner2 inner2 = new Inner2();
+			inner2.display();
+
+		}
+
 		Inner inner = new Inner();
 		inner.display();
 	}
 
 	// Внутренний класс
 	class Inner {
+
+		int innerY = 10;
 		
 		void display() {
 			
 			System.out.println("display() : outerX = " + outerX);
 		}
+	}
+
+	void showY() {
+		// System.out.println(innerY); // ошибка, переменная здесь неизвестна
 	}
 }
