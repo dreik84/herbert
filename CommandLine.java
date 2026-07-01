5c4da27aae88a16f442f99a853b3f9998bc3bbcc
dreik84 <dreik84@mail.ru>
2026-07-01 16:11:52 +0300

    Create CommandLine.java

---
+++ CommandLine.java
@@ -0,0 +1,8 @@
+// Отображение всех аргументов командной строки
+class CommandLine {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < args.length; i++) 
+			System.out.println("args[" + i + "]: " + args[i]);
+	}
+}
