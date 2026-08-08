9470a4c93a2397ded48bd5f3cd572e9e64978296
dreik84 <dreik84@mail.ru>
2026-08-08 16:03:31 +0300

    Update IFTest.java

--- IFTest.java
+++ IFTest.java
@@ -2,10 +2,56 @@
 class IFTest {
 	public static void main(String[] args) {
 		
-		IntStack stack = new FixedStack(10);
+		IntStack fixedStack = new FixedStack(10);
+		IntStack dynStack = new DynStack(10);
+
+		for (int i = 0; i < 12; i++) fixedStack.push(i);
+		for (int i = 0; i < 12; i++) dynStack.push(i);
+
+		for (int i = 0; i < 12; i++)
+			System.out.print(fixedStack.pop() + " ");
+
+		System.out.println();
+
+		for (int i = 0; i < 12; i++)
+                        System.out.print(dynStack.pop() + " ");
+
+		System.out.println();
 	}
 }
 
+// Реализация расширяемого стека
+class DynStack implements IntStack {
+	private int[] stck;
+	private int tos; // top of stack
+	
+	DynStack(int size) {
+		stck = new int[size];
+		tos = -1;
+	}
+
+	public void push(int item) {
+		if (tos == stck.length - 1) {
+			int[] temp = new int[stck.length * 2];
+
+			for (int i = 0; i < stck.length; i++)
+				temp[i] = stck[i];
+
+			stck = temp;
+		}
+
+		stck[++tos] = item;
+	}
+
+	public int pop() {
+		if (tos < 0) {
+			System.out.println("Стек пуст");
+			return 0;
+		}
+
+		return stck[tos--];
+	}
+}
 
 // Реализация IntStack фиксированной длины
 class FixedStack implements IntStack {
