2da8f52b5afbf75c657acb60e97fe25efde148d2
dreik84 <dreik84@mail.ru>
2026-08-23 16:12:43 +0300

    Create ThrowDemo.java

---
+++ ThrowDemo.java
@@ -0,0 +1,24 @@
+// Демонстрация применения throw
+class ThrowDemo {
+	
+	static void demoproc() {
+		
+		try {
+			throw new NullPointerException("Demo");
+		
+		} catch(NullPointerException e) {
+			System.out.println("Перехвачено внутри demoproc()");
+			throw e;
+		}	
+	}
+
+	public static void main(String[] args) {
+		
+		try {
+			demoproc();
+
+		} catch(NullPointerException e) {
+			System.out.println("Повторно перехвачено");
+		}
+	}
+}
