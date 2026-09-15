9db78ea6b4beb3dede549a6609993f243da93936
dreik84 <dreik84@mail.ru>
2026-09-15 16:27:08 +0300

    Create ThreadDemo.java

---
+++ ThreadDemo.java
@@ -0,0 +1,45 @@
+// Создание второго потока
+
+class ThreadDemo {
+	public static void main(String[] args) {
+		
+		NewThread nt = new NewThread();
+		nt.t.run();
+
+		try {
+			for (int i = 5; i > 0; i--) {
+				System.out.println("Главный поток: " + i);
+				Thread.sleep(1000);
+			}
+		} catch(InterruptedException e) {
+			System.out.println("Главный поток прерван");
+		}
+
+		System.out.println("Завершение главного потока");
+	}
+}
+
+class NewThread implements Runnable {
+	Thread t;
+
+	NewThread() {
+		t = new Thread(this, "Demo Thread");       // Создание потока
+		System.out.println("Дочерний поток " + t);
+	}
+
+	// Точка входа для второго потока
+	public void run() {
+		
+		try {
+			for (int i = 5; i > 0; i--) {
+				System.out.println("Дочерний поток: " + i);
+				Thread.sleep(500);		
+			}
+
+		} catch(InterruptedException e) {
+			System.out.println("Дочерний поток прерван");		
+		}
+
+		System.out.println("Завершение дочернего потока");
+	}
+}
